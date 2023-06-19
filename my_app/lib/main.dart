bf753a8ce7d62a5e3a48cb47336594c0cc6c7d25
d-dovale <ddovale2004@gmail.com>
2023-06-19 14:50:02 -0400

    Added an Icon, created a class for the Home Screen, and added assets folder to project.

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome to Sweat Smart"),
-          centerTitle: true,
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Welcome to Sweat Smart"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Icon(
+          Icons.fitness_center,
+          color: Color.fromARGB(255, 194, 32, 21),
+          size: 200.0,
         ),
       ),
-    ));
+    );
+  }
+}
