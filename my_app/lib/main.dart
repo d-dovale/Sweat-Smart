7b6714c3a0520e6befea67be98a875e31efbaad4
d-dovale <ddovale2004@gmail.com>
2023-06-22 12:21:18 -0400

    Copied new files from old folder

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/screens/home.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() {
+  runApp(MyApp());
+}
 
-class Home extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Welcome to Sweat Smart"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Icon(
-          Icons.fitness_center,
-          color: Color.fromARGB(255, 194, 32, 21),
-          size: 200.0,
-        ),
-      ),
+    return MaterialApp(
+      home: Home(),
     );
   }
 }
--- my_app/lib/screens/home.dart
+++ my_app/lib/screens/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 // Screen Background Color for Dark Mode: #191919
@@ -6,17 +8,26 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text("Welcome to Sweat Smart"),
-      //   centerTitle: true,
-      // ),
-      body: Center(
-        child: Icon(
-          Icons.fitness_center,
-          color: Color.fromARGB(255, 194, 32, 21),
-          size: 200.0,
-        ),
-      ),
-    );
+        backgroundColor: Color.fromARGB(255, 25, 25, 25),
+        body: Column(
+          children: <Widget>[
+            Text(
+              'SWEAT SMART',
+              style: TextStyle(
+                fontSize: 37.0,
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'POWERED BY CHATGPT',
+              style: TextStyle(
+                fontSize: 7.7,
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ));
   }
 }
