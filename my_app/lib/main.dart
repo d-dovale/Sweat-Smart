fc532de7f471f0095e7488793fa395ad4872bcdf
d-dovale <ddovale2004@gmail.com>
2023-06-26 13:07:08 -0400

    Changed android app name and changed name of screen to loading screen

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/screens/home.dart';
+import 'package:my_app/screens/loading.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: Loading(),
     );
   }
 }
--- my_app/lib/screens/loading.dart
+++ my_app/lib/screens/loading.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 // Screen Background Color for Dark Mode: #191919
 
-class Home extends StatelessWidget {
+class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
