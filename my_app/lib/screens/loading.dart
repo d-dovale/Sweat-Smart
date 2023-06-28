14fc3683b3373d865a6ec9249986905cb7fe8909
d-dovale <ddovale2004@gmail.com>
2023-06-28 13:16:02 -0400

    Pushed loading screen changes

--- my_app/lib/screens/loading.dart
+++ my_app/lib/screens/loading.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/screens/home.dart';
 import 'package:my_app/shared/navbar.dart';
@@ -17,14 +18,15 @@ class _LoadingState extends State<Loading> {
   }
 
   _navigatetohome() async {
-    await Future.delayed(Duration(seconds: 2), () {});
+    await Future.delayed(Duration(seconds: 2000), () {});
     Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => NavBar(
-            key: null,
-          ),
-        ));
+      context,
+      MaterialPageRoute(
+        builder: (context) => NavBar(
+          key: null,
+        ),
+      ),
+    );
   }
 
   @override
@@ -32,50 +34,62 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 25, 25, 25),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            double availableWidth = constraints.maxWidth;
+            double availableHeight = constraints.maxHeight;
+
+            double logoSize = availableWidth * 0.15;
+            double titleFontSize = availableWidth * 0.15;
+            double subtitleFontSize = availableWidth * 0.027;
+
+            return Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image(
-                  image: AssetImage('assets/images/Sweat Smart Logo.png'),
-                  width: 120.0,
-                  height: 120.0,
-                ),
-                Column(
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'SWEAT SMART',
-                      style: TextStyle(
-                        fontSize: 110.0,
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontFamily: 'BebasNeue',
-                        letterSpacing: 2.0,
-                      ),
+                  children: [
+                    Image(
+                      image: AssetImage('assets/images/Sweat Smart Logo.png'),
+                      width: logoSize,
+                      height: logoSize,
                     ),
-                    Text(
-                      'POWERED BY CHATGPT',
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 3.0,
-                        fontFamily: 'Lato',
-                      ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'SWEAT SMART',
+                          style: TextStyle(
+                            fontSize: titleFontSize,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontFamily: 'BebasNeue',
+                            letterSpacing: 2.0,
+                          ),
+                        ),
+                        Text(
+                          'POWERED BY CHATGPT',
+                          style: TextStyle(
+                            fontSize: subtitleFontSize,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 3.0,
+                            fontFamily: 'Lato',
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
-            Padding(
-                child: Image(
-                  image: AssetImage("assets/images/loading.gif"),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, availableHeight * 0.15, 0, 0),
+                  child: Image(
+                    image: AssetImage("assets/images/loading.gif"),
+                  ),
                 ),
-                padding: EdgeInsets.fromLTRB(0, 125, 0, 0))
-          ],
+              ],
+            );
+          },
         ),
       ),
     );
