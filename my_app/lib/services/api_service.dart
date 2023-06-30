ba329682821bf8a4c292508b6b0a89c2cf232369
d-dovale <ddovale2004@gmail.com>
2023-06-30 13:08:15 -0400

    API key change

--- my_app/lib/services/api_service.dart
+++ my_app/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
 
 class ApiService {
   static Future<void> getModels() async {
