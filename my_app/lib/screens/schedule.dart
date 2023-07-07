0e59884be75acc9590a334c84b724d063aa9e0bc
JacobHoppenstedt <jacobhoppenstedt@ufl.edu>
2023-07-07 12:54:38 -0400

    added chatgpt generation and fixed firebase error

--- my_app/lib/screens/schedule.dart
+++ my_app/lib/screens/schedule.dart
@@ -1,154 +1,228 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:my_app/shared/testmessages.dart';
-
 import '../services/api_service.dart';
+
+// class SchedulePage extends StatefulWidget {
+//   const SchedulePage({Key? key}) : super(key: key);
+
+//   @override
+//   State<SchedulePage> createState() => _SchedulePageState();
+// }
+
+// class _SchedulePageState extends State<SchedulePage> {
+//   bool _isTyping = false;
+
+//   late TextEditingController textEditingController;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     textEditingController = TextEditingController();
+//   }
+
+//   @override
+//   void dispose() {
+//     textEditingController.dispose();
+//     super.dispose();
+//   }
+
+//   List<ChatModel> chatList = [];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: SafeArea(
+//       child: Column(children: [
+//         Flexible(
+//           child: ListView.builder(
+//             itemCount: chatList.length,
+//             itemBuilder: (context, index) {
+//               return ChatWidget(
+//                 msg: chatList[index].msg,
+//                 chatIndex: chatList[index].chatIndex,
+//               );
+//             },
+//           ),
+//         ),
+//         if (_isTyping) ...[
+//           const SpinKitThreeBounce(
+//             color: Colors.black,
+//             size: 18,
+//           ),
+//           const SizedBox(
+//             height: 15,
+//           ),
+//         ],
+//         Material(
+//           color: const Color.fromARGB(255, 221, 220, 220),
+//           child: Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: Row(
+//               children: [
+//                 Expanded(
+//                   child: TextField(
+//                     style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+//                     controller: textEditingController,
+//                     onSubmitted: (value) async{
+//                       await sendMessage(modelId: "gpt-3.5-turbo");
+//                       },
+//                     decoration: const InputDecoration.collapsed(
+//                         hintText: "How can I help you",
+//                         hintStyle: TextStyle(color: Colors.grey)),
+//                   ),
+//                 ),
+//                 IconButton(
+//                     onPressed: () async{
+//                       await sendMessage(modelId: "gpt-3.5-turbo");
+//                       },
+//                     icon: const Icon(
+//                       Icons.send,
+//                       color: Colors.red,
+//                     ))
+//               ],
+//             ),
+//           ),
+//         )
+//       ]),
+//     ));
+//   }
+
+//   Future<void> sendMessage({required modelId}) async {
+//     try {
+//       setState(() {
+//         _isTyping = true;
+//       });
+//       ApiService.sendMessage(
+//           message: textEditingController.text,
+//           modelId: "gpt-3.5-turbo",
+//           );
+//       setState(() {});
+//     } catch (error) {
+//       print("error $error");
+//     } finally {
+//       setState(() {
+//         _isTyping = false;
+//       });
+//     }
+//   }
+// }
+
+// class ChatWidget extends StatelessWidget {
+//   const ChatWidget({super.key, required this.msg, required this.chatIndex});
+//   final String msg;
+//   final int chatIndex;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Material(
+//           color: chatIndex == 0
+//               ? const Color.fromARGB(255, 221, 220, 220)
+//               : Color.fromARGB(255, 236, 216, 216),
+//           child: Padding(
+//             padding: const EdgeInsets.all(8.0),
+//             child: Row(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 chatIndex == 0
+//                     ? Image.asset(
+//                         'assets/images/Buff Guy.png',
+//                         height: 30,
+//                         width: 30,
+//                       )
+//                     : Image.asset(
+//                         'assets/images/Sweat Smart Logo.png',
+//                         height: 30,
+//                         width: 30,
+//                       ),
+//                 Expanded(
+//                   child: Text(
+//                     msg,
+//                     style: const TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
 
 class SchedulePage extends StatefulWidget {
   const SchedulePage({Key? key}) : super(key: key);
 
   @override
-  State<SchedulePage> createState() => _SchedulePageState();
+  _SchedulePageState createState() => _SchedulePageState();
 }
 
 class _SchedulePageState extends State<SchedulePage> {
-  bool _isTyping = false;
+  final TextEditingController _textEditingController = TextEditingController();
+  final ApiService _apiService = ApiService();
+  List<Map<String, String>> messages = [];
+  bool isLoading = false;
 
-  late TextEditingController textEditingController;
+  Future<void> sendMessage() async {
+    final String message = _textEditingController.text;
+    if (message.trim().isEmpty) return;
 
-  @override
-  void initState() {
-    super.initState();
-    textEditingController = TextEditingController();
-  }
+    setState(() {
+      isLoading = true;
+    });
 
-  @override
-  void dispose() {
-    textEditingController.dispose();
-    super.dispose();
+    try {
+      await _apiService.sendMessage(message: message, modelId: "gpt-3.5-turbo");
+
+      setState(() {
+        messages = List<Map<String, String>>.from(_apiService.messages);
+      });
+    } catch (error) {
+      print("Error: $error");
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   elevation: 2,
-        //   leading: Padding(
-        //     padding: const EdgeInsets.all(8.0),
-        //     child: Image.asset('assets/images/Sweat Smart Logo.png'),
-        //   ),
-        //   backgroundColor: Colors.red[200], // Lighter red color
-        // ),
-        body: SafeArea(
-      child: Column(
+      appBar: AppBar(
+        title: const Text('Workout Schedule'),
+      ),
+      body: Column(
         children: [
-          Flexible(
+          Expanded(
             child: ListView.builder(
-              itemCount: 6,
+              itemCount: messages.length,
               itemBuilder: (context, index) {
-                return ChatWidget(
-                  msg: chatMessages[index]["msg"].toString(),
-                  chatIndex:
-                      int.parse(chatMessages[index]["chatIndex"].toString()),
+                final chat = messages[index];
+                return ListTile(
+                  title: Text(chat['role'] ?? ''),
+                  subtitle: Text(chat['content'] ?? ''),
                 );
               },
             ),
           ),
-          if (_isTyping) ...[
-            const SpinKitThreeBounce(
-              color: Colors.black,
-              size: 18,
-            ),
-            const SizedBox(
-              height: 15,
-            ),],
-            Material(
-              color: const Color.fromARGB(255, 221, 220, 220),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
-                        controller: textEditingController,
-                        onSubmitted: (value) {
-                          //TODO send message
-                        },
-                        decoration: const InputDecoration.collapsed(
-                            hintText: "How can I help you",
-                            hintStyle: TextStyle(color: Colors.grey)),
-                      ),
-                    ),
-                    IconButton(
-                        onPressed: () async {
-                          try {
-                            setState((){
-                              _isTyping = true;
-                            });
-                            print("Request sent");
-                            await ApiService.sendMessage(message: textEditingController.text, modelId: "gpt-3.5-turbo");
-                          } catch (error){
-                            print("error $error");
-                          }finally{
-                            setState((){
-                              _isTyping = false;
-                            });
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.send,
-                          color: Colors.red,
-                        ))
-                  ],
-                ),
-              ),
-            )
-          ]
-      ),
-    ));
-  }
-}
-
-class ChatWidget extends StatelessWidget {
-  const ChatWidget({super.key, required this.msg, required this.chatIndex});
-  final String msg;
-  final int chatIndex;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Material(
-          color: chatIndex == 0
-              ? const Color.fromARGB(255, 221, 220, 220)
-              : Color.fromARGB(255, 236, 216, 216),
-          child: Padding(
+          Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                chatIndex == 0
-                    ? Image.asset('assets/images/Buff Guy.png',
-                        height: 30,
-                        width: 30,
-                        )
-                    : Image.asset(
-                        'assets/images/Sweat Smart Logo.png',
-                        height: 30,
-                        width: 30,
-                      ),
-                Expanded(
-                  child: Text(
-                    msg,
-                    style: const TextStyle(fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
+            child: TextField(
+              controller: _textEditingController,
+              decoration: const InputDecoration(
+                hintText: 'Enter a message',
+              ),
             ),
           ),
-        ),
-      ],
+          ElevatedButton(
+            onPressed: isLoading ? null : sendMessage,
+            child: isLoading ? const CircularProgressIndicator() : const Text('Send'),
+          ),
+        ],
+      ),
     );
   }
 }
--- my_app/lib/services/api_service.dart
+++ my_app/lib/services/api_service.dart
@@ -5,9 +5,53 @@ import 'package:http/http.dart' as http;
 
 
 
+// class ApiService {
+//   final List<Map<String, String>> messages = [];
+//   static Future<void> sendMessage(
+//       {required String message, required String modelId}) async {
+//     try {
+//       var response = await http.post(
+//         Uri.parse("$BASE_URL/completions"),
+//         headers: {
+//           "Authorization": "Bearer $API_KEY",
+//           "Content-Type": "application/json"
+//         },
+//         body: jsonEncode({
+//           "model": "gpt-3.5-turbo",
+//           "messages": [
+//             {
+//               "role": "user",
+//               "content": "Give me a workout routine for a male $message",
+//             }
+//           ]
+//         }),
+//       );
+//       print(response.body);
+//       if(response.statusCode == 200){
+//         String content = 
+//         jsonDecode(response.body)["choices"][0]['message']['content'];
+//         content = content.trim();
+      
+        
+//       }
+
+//     } catch (error) {
+//       print("error $error");
+//     }
+//   }
+// }
+
+// class ChatModel {
+//   final String msg;
+//   final int chatIndex;
+
+//   ChatModel({required this.msg, required this.chatIndex});
+// }
+
 class ApiService {
-  static Future<void> sendMessage(
-      {required String message, required String modelId}) async {
+  final List<Map<String, String>> messages = [];
+
+  Future<void> sendMessage({required String message, required String modelId}) async {
     try {
       var response = await http.post(
         Uri.parse("$BASE_URL/completions"),
@@ -16,36 +60,30 @@ class ApiService {
           "Content-Type": "application/json"
         },
         body: jsonEncode({
-          "model": "gpt-3.5-turbo",
+          "model": modelId,
           "messages": [
             {
               "role": "user",
-              "content": "You are a helpful assistant $message",
+              "content": "Give me a workout routine for a male $message",
             }
           ]
         }),
       );
-      print(response.body);
-      if(response.statusCode == 200){
-        print("success");
-      }
-      // Map jsonResponse = jsonDecode(response.body);
 
-      // if (jsonResponse['error'] != null) {
-      //   throw HttpException(jsonResponse['error']["message"]);
-      // }
-      // if (jsonResponse["choices"].length > 0) {
-      //   print(jsonResponse["choices"][0]["text"]);
-      // }
+      if (response.statusCode == 200) {
+        String content = jsonDecode(response.body)["choices"][0]['message']['content'];
+        content = content.trim();
+
+        Map<String, String> newMessage = {
+          'role': 'chatbot',
+          'content': content,
+        };
+
+        // Add the new message to the messages list
+        messages.add(newMessage);
+      }
     } catch (error) {
-      print("error $error");
+      print("Error: $error");
     }
   }
-}
-
-class ChatModel {
-  final String msg;
-  final int chatIndex;
-
-  ChatModel({required this.msg, required this.chatIndex});
 }
