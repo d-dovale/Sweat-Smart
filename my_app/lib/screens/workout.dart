bb398b2bb1028794644582142cea8bd52c5589e4
JacobHoppenstedt <jacobhoppenstedt@ufl.edu>
2023-07-11 11:45:01 -0400

    fixed some shared pref stuff

--- my_app/lib/screens/workout.dart
+++ my_app/lib/screens/workout.dart
@@ -1,39 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// class WorkoutPage extends StatefulWidget {
-//   const WorkoutPage({super.key});
-
-//   @override
-//   State<WorkoutPage> createState() => _WorkoutPageState();
-// }
-
-// class _WorkoutPageState extends State<WorkoutPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Text(
-//             '[Split for the day]',
-//             style: TextStyle(
-//               fontFamily: 'BebasNeue',
-//               letterSpacing: 2.0,
-//             ),
-//           ),
-//           automaticallyImplyLeading: false,
-//           leading: IconButton(
-//             icon: const Icon(Icons.arrow_back_ios),
-//             onPressed: () {
-//               Navigator.pop(context);
-//             },
-//           ),
-//         ),
-//         body: Column(
-//           children: [],
-//         ));
-//   }
-// }
-
 class WorkoutPage extends StatefulWidget {
   const WorkoutPage({Key? key}) : super(key: key);
 
@@ -167,7 +134,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
               ),
               child: Text(
                 _selectedDate != null
-                    ? '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'
+                    ? '${_selectedDate!.month}/${_selectedDate!.day}/${_selectedDate!.year}'
                     : 'Select date',
                 style: const TextStyle(
                   fontSize: 16,
--- my_app/lib/services/api_service.dart
+++ my_app/lib/services/api_service.dart
@@ -4,50 +4,6 @@ import 'package:my_app/data/constants.dart';
 import 'package:http/http.dart' as http;
 
 
-
-// class ApiService {
-//   final List<Map<String, String>> messages = [];
-//   static Future<void> sendMessage(
-//       {required String message, required String modelId}) async {
-//     try {
-//       var response = await http.post(
-//         Uri.parse("$BASE_URL/completions"),
-//         headers: {
-//           "Authorization": "Bearer $API_KEY",
-//           "Content-Type": "application/json"
-//         },
-//         body: jsonEncode({
-//           "model": "gpt-3.5-turbo",
-//           "messages": [
-//             {
-//               "role": "user",
-//               "content": "Give me a workout routine for a male $message",
-//             }
-//           ]
-//         }),
-//       );
-//       print(response.body);
-//       if(response.statusCode == 200){
-//         String content = 
-//         jsonDecode(response.body)["choices"][0]['message']['content'];
-//         content = content.trim();
-      
-        
-//       }
-
-//     } catch (error) {
-//       print("error $error");
-//     }
-//   }
-// }
-
-// class ChatModel {
-//   final String msg;
-//   final int chatIndex;
-
-//   ChatModel({required this.msg, required this.chatIndex});
-// }
-
 class ApiService {
   final List<Map<String, String>> messages = [];
 
