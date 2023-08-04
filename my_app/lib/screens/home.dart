d4e6b50c09cdfac3115fef9867f5b89233e29f38
d-dovale <ddovale2004@gmail.com>
2023-08-04 12:29:28 -0400

    Updated the home navbar, home screen, and calendar to look nicer

--- my_app/lib/screens/home.dart
+++ my_app/lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:my_app/screens/workout.dart';
 import 'package:my_app/data/user.dart';
 import 'package:my_app/screens/information.dart';
 import 'package:my_app/shared/navbar.dart';
+import 'package:my_app/shared/title.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
@@ -19,50 +20,67 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 25, 25, 25),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Image(
-              image: AssetImage(
-                  'assets/images/Sweat Smart Loading Screen Light.gif'),
-              height: 400, //testing logo gif,
-            ),
-            Text(
-              'Welcome to Sweat Smart, ${widget.user.name}!',
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'BebasNeue',
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        double availableWidth = constraints.maxWidth;
+        double availableHeight = constraints.maxHeight;
+
+        double logoSize = availableWidth * 0.15;
+        double titleFontSize = availableWidth * 0.15;
+        double subtitleFontSize = availableWidth * 0.027;
+
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // const Image(
+              //   image: AssetImage(
+              //       'assets/images/Sweat Smart Loading Screen Light.gif'),
+              //   height: 400, //testing logo gif,
+              // ),
+              // Text(
+              //   'Welcome to Sweat Smart, ${widget.user.name}!',
+              //   textAlign: TextAlign.center,
+              //   style: const TextStyle(
+              //     fontSize: 24,
+              //     fontWeight: FontWeight.bold,
+              //     fontFamily: 'BebasNeue',
+              //   ),
+              // ),
+              AppTitle(
+                logoSize: logoSize,
+                titleFontSize: titleFontSize,
+                subtitleFontSize: subtitleFontSize,
+                textColor: Colors.white,
               ),
-            ),
-            const SizedBox(
-                height: 16), // Optional spacing between text and button
-            ElevatedButton(
-              onPressed: () {
-                // Handle button press
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return NavBar(
-                          user: widget.user); // Pass the User object to NavBar
-                    },
+              const SizedBox(
+                  height: 100), // Optional spacing between text and button
+              ElevatedButton(
+                onPressed: () {
+                  // Handle button press
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return NavBar(
+                            user:
+                                widget.user); // Pass the User object to NavBar
+                      },
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Click for Today\'s Workout, [workout variable]!',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'BebasNeue',
+                    letterSpacing: 2.0,
                   ),
-                );
-              },
-              child: const Text(
-                'Click for Today\'s Workout, [workout variable]!',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'BebasNeue',
-                  letterSpacing: 2.0,
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
--- my_app/lib/screens/schedule.dart
+++ my_app/lib/screens/schedule.dart
@@ -4,80 +4,6 @@ import 'package:table_calendar/table_calendar.dart';
 import '../services/api_service.dart';
 // ignore: depend_on_referenced_packages
 import 'package:intl/intl.dart';
-// class SchedulePage extends StatefulWidget {
-//   const SchedulePage({Key? key}) : super(key: key);
-
-//   @override
-//   _SchedulePageState createState() => _SchedulePageState();
-// }
-
-// class _SchedulePageState extends State<SchedulePage> {
-//   final TextEditingController _textEditingController = TextEditingController();
-//   final ApiService _apiService = ApiService();
-//   List<Map<String, String>> messages = [];
-//   bool isLoading = false;
-
-//   Future<void> sendMessage() async {
-//     final String message = _textEditingController.text;
-//     if (message.trim().isEmpty) return;
-
-//     setState(() {
-//       isLoading = true;
-//     });
-
-//     try {
-//       await _apiService.sendMessage(message: message, modelId: "gpt-3.5-turbo");
-
-//       setState(() {
-//         messages = List<Map<String, String>>.from(_apiService.messages);
-//       });
-//     } catch (error) {
-//       print("Error: $error");
-//     } finally {
-//       setState(() {
-//         isLoading = false;
-//       });
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Workout Schedule'),
-//       ),
-//       body: Column(
-//         children: [
-//           Expanded(
-//             child: ListView.builder(
-//               itemCount: messages.length,
-//               itemBuilder: (context, index) {
-//                 final chat = messages[index];
-//                 return ListTile(
-//                   title: Text(chat['role'] ?? ''),
-//                   subtitle: Text(chat['content'] ?? ''),
-//                 );
-//               },
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: TextField(
-//               controller: _textEditingController,
-//               decoration: const InputDecoration(
-//                 hintText: 'Enter a message',
-//               ),
-//             ),
-//           ),
-//           ElevatedButton(
-//             onPressed: isLoading ? null : sendMessage,
-//             child: isLoading ? const CircularProgressIndicator() : const Text('Send'),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 
 class SchedulePage extends StatefulWidget {
   const SchedulePage({Key? key}) : super(key: key);
@@ -101,69 +27,81 @@ class _SchedulePageState extends State<SchedulePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: content(),
-    );
-  }
-
-  Widget content() {
-    return Scaffold(
       backgroundColor: Color.fromARGB(255, 25, 25, 25),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            child: TableCalendar(
-              locale: 'en_US',
-              calendarFormat: _calendarFormat,
-              onFormatChanged: (format) {
-                setState(() {
-                  _calendarFormat = format;
-                });
-              },
-              focusedDay: _focusedDay,
-              firstDay: DateTime.utc(2010, 1, 1),
-              lastDay: DateTime.utc(2030, 1, 1),
-              selectedDayPredicate: (day) {
-                return isSameDay(_selectedDay, day);
-              },
-              onDaySelected: (selectedDay, focusedDay) {
-                setState(() {
-                  _selectedDay = selectedDay;
-                  _focusedDay = focusedDay;
-                });
-              },
-              headerStyle: HeaderStyle(
-                formatButtonVisible: false,
-                leftChevronIcon: Container(
-                  child: const Icon(
-                    Icons.chevron_left,
-                    color: Colors.red,
+          Center(
+            // Center the TableCalendar in the middle of the body
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+              child: TableCalendar(
+                locale: 'en_US',
+                calendarFormat: _calendarFormat,
+                onFormatChanged: (format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                },
+                focusedDay: _focusedDay,
+                firstDay: DateTime.utc(2010, 1, 1),
+                lastDay: DateTime.utc(2030, 1, 1),
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                },
+                headerStyle: HeaderStyle(
+                  formatButtonVisible: false,
+                  leftChevronIcon: Container(
+                    child: const Icon(
+                      Icons.chevron_left,
+                      color: Colors.red,
+                    ),
                   ),
-                ),
-                rightChevronIcon: Container(
-                  child: const Icon(
-                    Icons.chevron_right,
-                    color: Colors.red,
+                  rightChevronIcon: Container(
+                    child: const Icon(
+                      Icons.chevron_right,
+                      color: Colors.red,
+                    ),
                   ),
+                  titleTextStyle: TextStyle(
+                      color: Colors.white), // Set the month text color to white
                 ),
-              ),
-              calendarStyle: CalendarStyle(
-                todayDecoration: BoxDecoration(
-                  color: Colors.red,
-                  shape: BoxShape.circle,
-                ),
-                selectedDecoration: BoxDecoration(
-                  color: _selectedDay != null ? Colors.red[200] : Colors.red,
-                  shape: BoxShape.circle,
+                calendarStyle: CalendarStyle(
+                  todayDecoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  selectedDecoration: BoxDecoration(
+                    color: _selectedDay != null ? Colors.red[200] : Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  defaultTextStyle: TextStyle(color: Colors.white),
+                  weekendTextStyle: TextStyle(color: Colors.white),
+                  outsideTextStyle: TextStyle(color: Colors.grey),
+                  outsideDaysVisible: false,
+                  cellMargin: EdgeInsets.all(0),
+                  todayTextStyle: TextStyle(color: Colors.white),
                 ),
               ),
             ),
           ),
           if (_selectedDay != null)
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
               child: Text(
                 'On ${DateFormat.yMMMd().format(_selectedDay!)}, you have a back and bicep day!',
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: 'Lato',
+                  color: Colors.white,
+                ),
               ),
             ),
         ],
